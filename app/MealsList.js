bc12e9a75cd0a490446d11351475e9743c9d0ea4
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-03 18:56:36 +0530

    Update MealsList.js

--- app/MealsList.js
+++ app/MealsList.js
@@ -1,18 +1,22 @@
 'use client'
 import React from "react";
+const { meals, tickMeal } = useContext(MealsContext);
 
 const MealsList = () => {
-
     return (
         <div>
             <h2>Meals:</h2>
             <ul>
-                <li>
-                    <input 
-                        type="checkbox"
-                    />
-                    mealName
-                </li>
+                {meals.map((meal) => (
+                    <li key={meal.id}>
+                        <input 
+                            type="checkbox"
+                            checked={meal.ticked}
+                            onChange={() => tickMeal(meal.id)}
+                        />
+                        {meal.name}
+                    </li>
+                ))}
             </ul>
         </div>
     )
