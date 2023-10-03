0fe46a13c2ff97d95ceb9d1bfdb3c9b74823ea2e
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-03 20:05:43 +0530

    Update Counter.js

--- app/Counter.js
+++ app/Counter.js
@@ -7,7 +7,7 @@ const Counter = () => {
     const remainingMeals = meals.filter((meal) => !meal.ticked).length;
     return (
         <div>
-            <h3>Meals Remaining: {remaingMeals}</h3>
+            <h3>Meals Remaining: {remainingMeals}</h3>
         </div>
     )
 }
