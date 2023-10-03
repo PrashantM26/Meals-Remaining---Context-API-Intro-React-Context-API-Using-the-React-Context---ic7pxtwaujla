86cc98caa6e778089c962b0f6e008bdc75da2c49
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-03 20:03:11 +0530

    Update MealsList.js

--- app/MealsList.js
+++ app/MealsList.js
@@ -1,6 +1,6 @@
 'use client'
 import React, { useContext } from "react";
-import {MealsContext} from './MealsProvider';
+import { MealsContext } from './MealsProvider';
 
 const MealsList = () => {
     const { meals, tickMeal } = useContext(MealsContext);
