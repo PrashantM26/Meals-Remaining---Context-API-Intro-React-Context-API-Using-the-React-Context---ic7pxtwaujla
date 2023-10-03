6a75f918638757a6954bead15defb7fa987c0461
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-03 19:58:11 +0530

    Update Counter.js

--- app/Counter.js
+++ app/Counter.js
@@ -1,6 +1,6 @@
 'use client'
 import React, { useContext } from "react";
-import MealsContext from './MealsProvider';
+import {MealsContext} from './MealsProvider';
 
 const Counter = () => {
     const { meals } = useContext(MealsContext);
